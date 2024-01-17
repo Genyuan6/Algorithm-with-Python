c4e747d5bd7d38aa1620b052f669f8fbcc62cc16
Genyuan6 <141828953+Genyuan6@users.noreply.github.com>
2024-01-17 09:58:21 -0500

    Create 345. 反转字符串中的元音字母.py

---
+++ 345. 反转字符串中的元音字母.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = "aeiouAEIOU"
+        s_list = list(s)
+        i , j = 0, len(s_list) - 1
+        while i < j:
+            while i < j and s_list[i] not in vowels:
+                i += 1
+            while i < j and s_list[j] not in vowels:
+                j -= 1
+            s_list[i], s_list[j] = s_list[j], s_list[i]
+            i += 1
+            j -= 1
+        return "".join(s_list)
