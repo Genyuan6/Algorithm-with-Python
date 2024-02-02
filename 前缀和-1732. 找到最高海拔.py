58c325ed742790b24b7cbfb1e67bf9d2cb3bdc01
Genyuan6 <141828953+Genyuan6@users.noreply.github.com>
2024-02-02 14:32:27 -0500

    前缀和-1732. 找到最高海拔.py

---
+++ 前缀和-1732. 找到最高海拔.py
@@ -0,0 +1,22 @@
+# 有一个自行车手打算进行一场公路骑行，这条路线总共由 n + 1 个不同海拔的点组成。自行车手从海拔为 0 的点 0 开始骑行。
+
+# 给你一个长度为 n 的整数数组 gain ，其中 gain[i] 是点 i 和点 i + 1 的 净海拔高度差（0 <= i < n）。请你返回 最高点的海拔 。
+# 示例 1：
+# 输入：gain = [-5,1,5,0,-7]
+# 输出：1
+# 解释：海拔高度依次为 [0,-5,-4,1,1,-6] 。最高海拔为 1 。
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        # alttitute = []
+        # alttitute.append(0)
+        # n = len(gain)
+        # for i in range(n):
+        #     gain[i] += alttitute[i]
+        #     alttitute.append(gain[i])
+        # alttitute.sort()
+        # return alttitute[n]
+        highest = current = 0
+        for g in gain:
+            current += g
+            highest = max(highest, current)
+        return highest
