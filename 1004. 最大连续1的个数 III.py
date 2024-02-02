93e30dbb635f5385f3364334a0e3daeb2e776f5b
Genyuan6 <141828953+Genyuan6@users.noreply.github.com>
2024-02-02 00:23:48 -0500

    Create 1004. 最大连续1的个数 III.py

---
+++ 1004. 最大连续1的个数 III.py
@@ -0,0 +1,20 @@
+# 给定一个二进制数组 nums 和一个整数 k，如果可以翻转最多 k 个 0 ，则返回 数组中连续 1 的最大个数 。
+# 示例 1：
+# 输入：nums = [1,1,1,0,0,0,1,1,1,1,0], K = 2
+# 输出：6
+# 解释：[1,1,1,0,0,1,1,1,1,1,1]
+# 粗体数字从 0 翻转到 1，最长的子数组长度为 6。
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        zero_count = 0
+        max_length = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+            while zero_count > k:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+            max_length = max(max_length, right - left + 1)
+        return max_length
