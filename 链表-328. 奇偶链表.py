afedd065aac8e3a2ff0172f9c44aa120a1906798
Genyuan Liu <141828953+Genyuan6@users.noreply.github.com>
2024-02-06 17:44:18 -0500

    链表-328. 奇偶链表.py

---
+++ 链表-328. 奇偶链表.py
@@ -0,0 +1,27 @@
+给定单链表的头节点 head ，将所有索引为奇数的节点和索引为偶数的节点分别组合在一起，然后返回重新排序的列表。
+第一个节点的索引被认为是 奇数 ， 第二个节点的索引为 偶数 ，以此类推。
+请注意，偶数组和奇数组内部的相对顺序应该与输入时保持一致。
+你必须在 O(1) 的额外空间复杂度和 O(n) 的时间复杂度下解决这个问题。
+示例
+输入: head = [1,2,3,4,5]
+输出: [1,3,5,2,4]
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        odd = head
+        even = head.next
+        Headeven = even #先保留下even的头节点，在最后的时候要与odd的尾节点相连
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = Headeven
+        return head
