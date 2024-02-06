2042b4d3e5ddb594791135e90362f27f29e98b65
Genyuan6 <141828953+Genyuan6@users.noreply.github.com>
2024-02-06 00:21:35 -0500

    ★栈-394. 字符串解码.py

---
+++ 栈-394. 字符串解码.py
@@ -0,0 +1,42 @@
+# 有难度
+给定一个经过编码的字符串，返回它解码后的字符串。
+编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
+你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
+此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
+
+示例 1：
+输入：s = "3[a]2[bc]"
+输出："aaabcbc"
+示例 2：
+
+输入：s = "3[a2[c]]"
+输出："accaccacc"
+示例 3：
+
+输入：s = "2[abc]3[cd]ef"
+输出："abcabccdcdcdef"
+示例 4：
+
+输入：s = "abc3[cd]xyz"
+输出："abccdcdcdxyz"
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        cur_num = 0
+        cur_string = ""
+        for c in s:
+            if c == '[':
+                stack.append(cur_string)
+                stack.append(cur_num)
+                cur_string = ""
+                cur_num = 0
+            elif c == ']':
+                num = stack.pop()
+                prev_string = stack.pop()
+                cur_string = prev_string + num * cur_string
+            elif c.isdigit():
+                cur_num = cur_num * 10 + int(c)
+            else:
+                cur_string += c
+        return cur_string
