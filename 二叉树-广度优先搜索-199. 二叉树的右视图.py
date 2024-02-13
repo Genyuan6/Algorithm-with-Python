41c8f09aacb801a4a1a4574b7b582f5c30124068
Genyuan Liu <141828953+Genyuan6@users.noreply.github.com>
2024-02-13 16:22:14 -0500

    二叉树-广度优先搜索-199. 二叉树的右视图.py

---
+++ 二叉树-广度优先搜索-199. 二叉树的右视图.py
@@ -0,0 +1,27 @@
+给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+示例 1:
+输入: [1,2,3,null,5,null,4]
+输出: [1,3,4]
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        result = []
+        queue = [root]
+        while queue:
+            level_length = len(queue)
+            for i in range(level_length):
+                node = queue.pop(0)
+                if i == level_length - 1:
+                    result.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return result
